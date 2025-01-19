e1a610f729e46d69cdd9380f7c95d34c86780966
Dmitry <79811924115@yandex.ru>
2025-01-19 21:51:49 +0300

    feat: 04 ok

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+  if (err) {
+    console.log('error: ' + err.message);
+  }
+  console.log('directiry maked');
+});
+
+fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+  console.log(files);
+  files.forEach((file) => {
+    const sorseFilPath = path.join(__dirname, 'files', file);
+    const copiedFilePath = path.join(__dirname, 'files-copy', file);
+    fs.copyFile(sorseFilPath, copiedFilePath, (err) => {
+      if (err) {
+        console.log('Error: ' + err.message);
+      }
+    });
+  });
+});
